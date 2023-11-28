a1387a83cefe33d9d7ea6d740e371e49c855a460
Cornato <dragonroar0099@gmail.com>
2023-11-28 23:14:21 +0200

    test

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-def uppercase(str):
-    return ord('A') <= ord(str) <= ord('Z')
+def uppercase(s):
+    result = ""
+    index = 0
+
+    while index < len(s):
+        char = s[index]
+
+        if 'a' <= char <= 'z':
+            result += '{}'.format(chr(ord(char) - 32))
+        else:
+            result += char
+
+        index += 1
+
+    print(result)
