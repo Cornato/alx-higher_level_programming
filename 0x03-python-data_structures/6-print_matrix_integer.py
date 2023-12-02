dedd19908c0666e67ca73d0e89f5f2001663f2c5
Cornato <dragonroar0099@gmail.com>
2023-12-03 00:37:41 +0200

    test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
     for row in matrix:
-        for i, num in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(num), end="")
+        for i in row:
+            if i == len(row):
+                print("{:d}".format(i), end="")
             else:
-                print("{:d} ".format(num), end="")
+                print("{:d} ".format(i), end="")
         print()
