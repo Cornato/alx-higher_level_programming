fa1b15c99a3ea4399676c31ca54f1e4252d4cacf
Cornato <dragonroar0099@gmail.com>
2023-12-07 00:26:54 +0200

    test

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+    nList = []
+    for i in my_list:
+        if i == search:
+            nList.append(replace)
+        else:
+            nList.append(i)
+    return nList
