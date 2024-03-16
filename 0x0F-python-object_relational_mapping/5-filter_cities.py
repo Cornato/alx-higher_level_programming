f29f9b050247c036daf415a19fe9ec806e1176a2
Cornato <dragonroar0099@gmail.com>
2024-03-16 14:50:31 +0200

    tes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     query = dbnames.cursor()
     query.execute(
         "SELECT cities.id, cities.name\
-                  FROM cities INNER JOIN states ON states.id=cities.state_id\
+                  FROM cities INNER JOIN states ON cities.state_id=states.id\
                   WHERE states.name = %s",
         [argv[4]],
     )
