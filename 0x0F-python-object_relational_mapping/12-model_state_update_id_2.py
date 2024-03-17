bfe924cb2d460946f0983affd9c0dd9e85d5da57
Cornato <dragonroar0099@gmail.com>
2024-03-17 14:09:06 +0200

    test

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that update the state name
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+
+    Session = sessionmaker(bind=engine)()
+    Base.metadata.create_all(engine)
+
+    state = Session.query(State).filter_by(id="2").first()
+    state.name = "New Mexico"
+    Session.commit()
+    Session.close()
